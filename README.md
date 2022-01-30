3f2f7b0d2b6c6b1bdba63a2996d0811ba61c9c26
970M <970M@protonmail.com>
2022-01-30 18:39:10 -0400

    V2.0.0 : Solution avec calcul de la position de l'élément dès le début

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # tr-Js-Menu-Collant
 
-## Description 
+## Description
+
 Création d'un menu collant en Javascript
 https://grafikart.fr/tutoriels/menu-collant-108#autoplay
 
 ## Version
 
 V1.0.0 : Solution naïve
+V2.0.0 : Solution avec calcul de la position de l'élément dès le début
 
+1. Sortie du selecteur
+2. Calcule de la position de l'élément par rapport au haut de la fenêtre dès le début et le comparer par rapport au scroll de la fenêtre
+3. Ajout de combinatoire pour diminuer la fréquence d'appel de Add et Remove Class
 
 ## BUG
 
-B1 : Creation d'un clignotement
-B2 : Pb. de performance, le code du scroll est lancé à chaque fois
+B3 : La largeur du menu diminue lorsqu'il passe en position fixed
--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 (function () {
+    // Quantité de scroll en pixel
+    // https://developer.mozilla.org/fr/docs/Web/API/Window/scrollY
+    var scrollY = function () {
+        var supportPageOffset = window.pageXOffset !== undefined;
+        var isCSS1Compat = (document.compatMode || "") === "CSS1Compat";
+
+        return supportPageOffset
+            ? window.pageYOffset
+            : isCSS1Compat
+            ? document.documentElement.scrollTop
+            : document.body.scrollTop;
+    };
+
     /* Lorsque que l'on scroll :
         Si le menu sort de l'ecran alors il deviendra fixe
     */
+    var element = document.querySelector(".menu");
+    var top = element.getBoundingClientRect().top + scrollY();
+    console.log("top=", top);
+    // Detecter la position du menu par rapport à l'écran : element.getBoundingClientRect()
+    console.log("pos menu=", element.getBoundingClientRect().top);
 
     var onScroll = function () {
-        var element = document.querySelector(".menu");
-
-        // Detecter la position du menu par rapport à l'écran : element.getBoundingClientRect()
-        console.log(element.getBoundingClientRect().top);
-
-        if (element.getBoundingClientRect().top < 0) {
-            
-            /* 
-                element.style.position = "fixed";
-                element.style.top = "0px";
-            */
-
-            // --- Meilleur solution
+        var hasScrollClass = element.classList.contains("fixed");
+        // if (element.getBoundingClientRect().top <= 0) {
+        if (scrollY() > top && !hasScrollClass) {
+            console.log("Add");
             // Ajouter une class fixed
             element.classList.add("fixed");
-            
-        } else {
-
+        } else if (scrollY() <= top && hasScrollClass) {
+            console.log("Remove");
             // Supprimer la class fixed
             element.classList.remove("fixed");
-
         }
     };
     window.addEventListener("scroll", onScroll);
