bad885f59777927251aa88f7644f300090f1c9a2
970M <970M@protonmail.com>
2022-01-30 21:13:13 -0400

    V3.0.0 : Solution fonctionnelle et responsive

--- README.md
+++ README.md
@@ -7,14 +7,14 @@ https://grafikart.fr/tutoriels/menu-collant-108#autoplay
 
 ## Version
 
-V1.0.0 : Solution naïve
-V2.0.0 : Solution avec calcul de la position de l'élément dès le début
+V1.0.0 : Solution naïve  
+V2.0.0 : Solution avec calcul de la position de l'élément dès le début  
 V2.1.0 : Solution satisfaisante pour le menu et le texte
 
     1. Correction de la diminution de la largeur du menu lors du scroll
     2. Ajout d'un faux élément pour remplacer le menu sous la topbar lorsque le menu est en position fixed (passage du texte sous l'élèment)
     3. Suppression margin padding sur body
 
-## BUG
+V3.0.0 : Solution fonctionnelle et responsive
 
-B4. La page n'est pas responsive, redimentionement à gérer
+## BUG
--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 (function () {
     // Quantité de scroll en pixel
     // https://developer.mozilla.org/fr/docs/Web/API/Window/scrollY
-    var scrollY = function () {
-        var supportPageOffset = window.pageXOffset !== undefined;
-        var isCSS1Compat = (document.compatMode || "") === "CSS1Compat";
+    // var scrollY = function () {
+    //     var supportPageOffset = window.pageXOffset !== undefined;
+    //     var isCSS1Compat = (document.compatMode || "") === "CSS1Compat";
 
-        return supportPageOffset
-            ? window.pageYOffset
-            : isCSS1Compat
-            ? document.documentElement.scrollTop
-            : document.body.scrollTop;
+    //     return supportPageOffset
+    //         ? window.pageYOffset
+    //         : isCSS1Compat
+    //         ? document.documentElement.scrollTop
+    //         : document.body.scrollTop;
+    // };
+
+    var scrollY = function () {
+        return window.scrollY;
     };
 
-    /* Lorsque que l'on scroll :
-        Si le menu sort de l'ecran alors il deviendra fixe
-    */
-
+    // --- Variables
     // Calculs initials
     var element = document.querySelector(".menu");
     var rect = element.getBoundingClientRect();
-    var top = rect.top + scrollY();
+    var top = rect.top + window.scrollY;
 
     // Creer un élément fake
     var fake = document.createElement("div");
     fake.style.with = rect.width + "px";
     fake.style.height = rect.heigth + "px";
 
+    // --- Fonctions
     var onScroll = function () {
         var hasScrollClass = element.classList.contains("fixed");
         // if (element.getBoundingClientRect().top <= 0) {
-        if (scrollY() > top && !hasScrollClass) {
+        if (window.scrollY > top && !hasScrollClass) {
             console.log("Add");
 
             // Ajouter une class fixed
@@ -37,12 +39,28 @@
             element.style.width = rect.width + "px";
             // Recuperer l'élément parent et lui ajout fake avant element
             element.parentNode.insertBefore(fake, element);
-        } else if (scrollY() <= top && hasScrollClass) {
+        } else if (window.scrollY <= top && hasScrollClass) {
             console.log("Remove");
             // Supprimer la class fixed
             element.classList.remove("fixed");
             element.parentNode.removeChild(fake);
         }
     };
+
+    var onResize = function () {
+        // Défaire le style
+        element.style.width = "auto";
+        element.classList.remove("fixed");
+        fake.style.display = "none";
+        // Recalculer les positions
+        rect = element.getBoundingClientRect();
+        top = rect.top + window.scrollY;
+        // Remettre le style
+        fake.style.with = rect.width + "px";
+        fake.style.height = rect.height + "px";
+        onScroll();
+    };
+    // --- Listener
     window.addEventListener("scroll", onScroll);
+    window.addEventListener("resize", onResize);
 })();
